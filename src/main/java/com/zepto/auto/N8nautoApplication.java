ed905c96fe73bd59a3ef5f6e76effde24ba86df9
Jaya Prakash M <123569737+JayaPrakash112003@users.noreply.github.com>
2026-01-20 10:59:01 +0530

    Update N8nautoApplication.java

--- src/main/java/com/zepto/auto/N8nautoApplication.java
+++ src/main/java/com/zepto/auto/N8nautoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZeptoAutomationApplication {
+public class N8nautoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZeptoAutomationApplication.class, args);
     }
